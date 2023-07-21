78853e9e5ba9b2ade6b8b4bc34b043a49a0b4416
gloire <gloirengayo@gmail.com>
2023-07-21 23:03:05 +0100

    Add text footer

--- application/views/template/footer.php
+++ application/views/template/footer.php
@@ -1,5 +1,5 @@
-<footer>
-        je suis le footer
+    <footer>
+        <h2>Je suis le footer se trouvant dans le template</h2>
     </footer>
 
     <script src='<?= js_url('bootstrap.bundle.min.js') ?>'></script>
